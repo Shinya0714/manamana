df9261127d677f51d901a25a9437d7d3dfcf7123
Shinya0714 <doradoranobinobi93871293@yahoo.co.jp>
2022-04-24 14:35:13 +0900

    メソッドの移動

--- go/app/general/general.go
+++ go/app/general/general.go
@@ -2,9 +2,39 @@ package general
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
+	"time"
 )
 
-func General() {
+func CheckBookoBuildingPossible(bookBuildingString string) string {
 
-	fmt.Printf("from general.go update2")
+	bookoBuildingPossible := "false"
+
+	t := time.Now()
+
+	today := t.Format("20060102")
+
+	if bookBuildingString != "---" {
+
+		fromMonthint, _ := strconv.Atoi(strings.Split(strings.Split(bookBuildingString, "-")[0], "/")[0])
+		fromDayint, _ := strconv.Atoi(strings.Split(strings.Split(bookBuildingString, "-")[0], "/")[1])
+
+		toMonthint, _ := strconv.Atoi(strings.Split(strings.Split(bookBuildingString, "-")[1], "/")[0])
+		toDayint, _ := strconv.Atoi(strings.Split(strings.Split(bookBuildingString, "-")[1], "/")[1])
+
+		fromDate := strconv.Itoa(t.Year()) + fmt.Sprintf("%02d", fromMonthint) + fmt.Sprintf("%02d", fromDayint)
+		toDate := strconv.Itoa(t.Year()) + fmt.Sprintf("%02d", toMonthint) + fmt.Sprintf("%02d", toDayint)
+
+		toDayInt, _ := strconv.Atoi(today)
+		fromDateInt, _ := strconv.Atoi(fromDate)
+		toDateInt, _ := strconv.Atoi(toDate)
+
+		if fromDateInt <= toDayInt && toDayInt <= toDateInt {
+
+			bookoBuildingPossible = "true"
+		}
+	}
+
+	return bookoBuildingPossible
 }
