c9371f26b4b310e2c6c79ddd83c8dce756342b58
Shinya0714 <doradoranobinobi93871293@yahoo.co.jp>
2022-10-05 17:00:27 +0900

    メソッド追加

--- go/app/general/general.go
+++ go/app/general/general.go
@@ -2,25 +2,26 @@ package general
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
 	"strconv"
 	"strings"
 	"time"
 )
 
-func GetStatusMap() map[string]int {
+// func GetStatusMap() map[string]int {
 
-	m := map[string]int{
-		"CANCEL":            0,
-		"NO_HANDLING":       1,
-		"AVAILABLE":         2,
-		"WITHIN_THE_PERIOD": 3,
-		"OUT_OF_TERM":       4,
-		"RESERVED":          5,
-		"NO_RESERVATION":    6,
-	}
+// 	m := map[string]int{
+// 		"CANCEL":            0,
+// 		"NO_HANDLING":       1,
+// 		"AVAILABLE":         2,
+// 		"WITHIN_THE_PERIOD": 3,
+// 		"OUT_OF_TERM":       4,
+// 		"RESERVED":          5,
+// 		"NO_RESERVATION":    6,
+// 	}
 
-	return m
-}
+// 	return m
+// }
 
 func CheckBookoBuildingPossible(bookBuildingString string) string {
 
@@ -55,4 +56,15 @@ func CheckBookoBuildingPossible(bookBuildingString string) string {
 	}
 
 	return bookoBuildingPossible
+}
+
+func loadEnv() {
+
+	err := godotenv.Load("../.env")
+	if err != nil {
+		fmt.Printf("読み込み出来ませんでした: %v", err)
+	} else {
+
+		fmt.Printf(".env読み込み完了")
+	}
 }
